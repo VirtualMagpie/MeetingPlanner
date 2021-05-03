d0f11847c7e01754bdb887e8c1122212cfae32f6
VirtualMagpie <>
2021-05-03 09:25:19 +0200

    Add ReservationDbService

---
+++ backend/meeting-planner/src/main/java/com/example/meetingplanner/service/db/ReservationDbService.java
@@ -0,0 +1,68 @@
+package com.example.meetingplanner.service.db;
+
+import com.example.meetingplanner.entity.MaterielMobileDb;
+import com.example.meetingplanner.entity.ReservationDb;
+import com.example.meetingplanner.entity.SalleDb;
+import com.example.meetingplanner.entity.TypeReunionDb;
+import com.example.meetingplanner.model.Materiel;
+import com.example.meetingplanner.model.Reservation;
+import com.example.meetingplanner.repository.MaterielMobileRepository;
+import com.example.meetingplanner.repository.ReservationRepository;
+import com.example.meetingplanner.repository.SalleRepository;
+import com.example.meetingplanner.repository.TypeReunionRepository;
+import com.example.meetingplanner.utils.converter.ReservationConverter;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
+public class ReservationDbService {
+
+  private final ReservationRepository reservationRepository;
+  private final SalleRepository salleRepository;
+  private final TypeReunionRepository typeReunionRepository;
+  private final MaterielMobileRepository materielMobileRepository;
+
+  public Set<Reservation> fetchAll() {
+    return reservationRepository.findAll().stream()
+        .map(ReservationConverter::fromDb)
+        .collect(Collectors.toSet());
+  }
+
+  /**
+   * Save a new reservation in db. Is not supposed to save or edit other joined entites (salle,
+   * materiel, etc...)
+   */
+  public Reservation save(Reservation reservation) {
+
+    // Salle must already exist
+    SalleDb salleDb = salleRepository.findById(reservation.getSalle().getId()).orElseThrow();
+
+    // Type reunion must already exist
+    TypeReunionDb typeReunionDb =
+        typeReunionRepository.findById(reservation.getTypeReunion().getId()).orElseThrow();
+
+    // Materiels must already exist
+    Set<MaterielMobileDb> materielMobileDbs =
+        reservation.getMaterielsMobiles().stream()
+            .map(Materiel::getId)
+            .map(materielMobileRepository::findById)
+            .map(Optional::orElseThrow)
+            .collect(Collectors.toSet());
+
+    // Saving entity
+    ReservationDb reservationDb = new ReservationDb();
+    reservationDb.setId(reservation.getId()); // TODO: maybe allow only creation, not edition?
+    reservationDb.setIdReservateur(reservation.getIdReservateur());
+    reservationDb.setSalle(salleDb);
+    reservationDb.setTypeReunion(typeReunionDb);
+    reservationDb.setDatetimeDebut(reservation.getDebut());
+    reservationDb.setDatetimeFin(reservation.getFin());
+    reservationDb.setMaterielReserve(materielMobileDbs);
+    return ReservationConverter.fromDb(reservationRepository.save(reservationDb));
+  }
+}
