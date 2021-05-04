95d5fda10301a00a747c9984d6fcc64d4d60d889
VirtualMagpie <>
2021-05-04 18:29:33 +0200

    Add integration test scenarios + missing routes in controller

--- backend/meeting-planner/src/main/java/com/example/meetingplanner/controller/MainController.java
+++ backend/meeting-planner/src/main/java/com/example/meetingplanner/controller/MainController.java
@@ -1,12 +1,14 @@
 package com.example.meetingplanner.controller;
 
 import com.example.meetingplanner.model.Reservation;
+import com.example.meetingplanner.model.TypeReunion;
 import com.example.meetingplanner.model.request.ReservationCreateRequest;
 import com.example.meetingplanner.service.MainBookingService;
+import com.example.meetingplanner.service.db.ReservationDbService;
+import com.example.meetingplanner.service.db.TypeReunionDbService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 import java.util.Set;
 
 @AllArgsConstructor
@@ -14,27 +16,29 @@ import java.util.Set;
 @RequestMapping
 public class MainController {
 
-    private final MainBookingService mainBookingService;
+  private final TypeReunionDbService typeReunionDbService;
+  private final ReservationDbService reservationDbService;
+  private final MainBookingService mainBookingService;
 
-    @GetMapping("/reservation/list")
-    public Set<Reservation> getReservationList() {
-        // TODO: to implement
-        return Collections.emptySet();
-    }
+  @GetMapping("/type-reunion/list")
+  public Set<TypeReunion> getTypeReunionList() {
+    return typeReunionDbService.fetchAll();
+  }
 
-    // TODO: ajouter récupération des types de réunion
-    // TODO: ajouter récupération des types de matériel ?
+  @GetMapping("/reservation/list")
+  public Set<Reservation> getReservationList() {
+    return reservationDbService.fetchAll();
+  }
 
-    @PostMapping("/reservation/create")
-    public Reservation postReservationCreate(@RequestBody ReservationCreateRequest body) {
-        // TODO: get idReservateur from http header instead
-        String idReservateur = "foobar";
-        return mainBookingService.findAndBook(
-                body.getIdTypeReunion(),
-                body.getNombrePersonne(),
-                body.getDebut(),
-                body.getFin(),
-                idReservateur
-        );
-    }
+  @PostMapping("/reservation/create")
+  public Reservation postReservationCreate(@RequestBody ReservationCreateRequest body) {
+    // TODO: get idReservateur from http header instead
+    String idReservateur = "foobar";
+    return mainBookingService.findAndBook(
+        body.getIdTypeReunion(),
+        body.getNombrePersonne(),
+        body.getDebut(),
+        body.getFin(),
+        idReservateur);
+  }
 }
